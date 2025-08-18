55a48c9b1426887f006b712c8afe41e181d297a7
fauzinashrullah <fauzimalik08@gmail.com>
2025-08-18 18:33:44 +0700

    chore: initial project setup for java ecommerce

---
+++ src/main/java/com/ecommerce/fauzi/FauziApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fauzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FauziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FauziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/fauzi/FauziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fauzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FauziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
